7ab2559d244c6c26b69eb2aad7502b65aab19d09
ShockedWhistle <darrellcrochet3@gmail.com>
2022-04-09 00:10:37 -0600

    Continued to take notes on functions and made a new file for notes on arrow operators.

--- DataStructors/Pointers.cpp
+++ DataStructors/Pointers.cpp
@@ -22,6 +22,15 @@ int main(){
 
     IncrementWithPointer(p); // Increments the integer associated with the pointer p despite the function not returning anything
 
+    int b[5]; // Stored in memory as one big block and since ints are 4 bytes each then you can use a pointer and a bit of math to get diffrent numbers stored in the array
+    b[0] = 0;
+
+    int c[5][5]; // These are stored as different arrays so 
+    int *j[5] = b; // This will return a pointer to a 1 D array of 5 ints
+    // j + 1  or  &b[1]  or  *(b + 1) would then return the address of the next array in the 2 D array
+    // *(b + 1) + 2  or  b[1] + 2  or  &b[1][2] would then return the address of the integer in the second array in the 2 D array index of 2 so it would be the same as accesing [1][2] in the base array
+    // *(&b + 1) would then return the int of [0][1]
+
     
 
     return 0;
