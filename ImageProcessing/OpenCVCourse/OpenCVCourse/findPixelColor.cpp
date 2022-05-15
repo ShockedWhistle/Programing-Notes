897b20d0d53eaf13adcdf60ebf7f362936f951fe
ShockedWhistle <darrellcrochet3@gmail.com>
2022-05-15 01:56:13 -0600

    Took notes and learned how to Select pixels and capture a window

---
+++ ImageProcessing/OpenCVCourse/OpenCVCourse/findPixelColor.cpp
@@ -0,0 +1,51 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+// Color Detection
+
+void colorDec() {
+	string path = "Resources/lambo.png";
+	Mat img = imread(path);
+	Mat imgHSV, mask, nrgb;
+	Vec3b RGB = img.at<Vec3b>(300, 300);
+	nrgb = imread(path);
+
+	for (int i = 0; i < nrgb.rows; i++) {
+		for (int r = 0; r < nrgb.cols; r++) {
+			nrgb.at<Vec3b>(i, r) = RGB;
+		}
+	}
+
+	cvtColor(img, imgHSV, COLOR_BGR2HSV);
+	int hmin = 0, smin = 110, vmin = 153;
+	int hmax = 19, smax = 240, vmax = 255;
+
+	// Used to find values for the color mask
+	namedWindow("Trackbars", (640, 200));
+	createTrackbar("Hue Min", "Trackbars", &hmin, 179);
+	createTrackbar("Hue Max", "Trackbars", &hmax, 179);
+	createTrackbar("Sat Min", "Trackbars", &smin, 255);
+	createTrackbar("Sat Max", "Trackbars", &smax, 255);
+	createTrackbar("Val Min", "Trackbars", &vmin, 255);
+	createTrackbar("Val Max", "Trackbars", &vmax, 255);
+
+	// Used so the track bars update the image indefinitly
+	while (true) {
+
+		Scalar lower(hmin, smin, vmin);
+		Scalar upper(hmax, smax, vmax);
+		inRange(imgHSV, lower, upper, mask); // Makes a color mask of all the pixels in the lower to upper ramge from the HSV image and puts it in the mask image
+
+		imshow("Image", img);
+		imshow("HSV", imgHSV);
+		imshow("Mask", mask);
+		imshow("New Color", nrgb);
+
+		waitKey(1);
+	}
+}
---
+++ ImageProcessing/OpenCVCourse/OpenCVCourse/targetWindow.cpp
@@ -0,0 +1,75 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+Mat getMat(HWND hwnd) {
+	HDC deviceContext = GetDC(hwnd);
+	HDC memoryDeviceContext = CreateCompatibleDC(deviceContext);
+
+	RECT windowRect;
+	GetClientRect(hwnd, &windowRect);
+
+	int height = windowRect.bottom;
+	int width = windowRect.right;
+
+	HBITMAP bitmap = CreateCompatibleBitmap(deviceContext, width, height);
+
+	SelectObject(memoryDeviceContext, bitmap);
+
+	// Copy data into bitmap
+	BitBlt(memoryDeviceContext, 0, 0, width, height, deviceContext, 0, 0, SRCCOPY);
+
+	// Spesify format by using bitmapinfoheader
+	BITMAPINFOHEADER bi;
+	bi.biSize = sizeof(BITMAPINFOHEADER);
+	bi.biWidth = width;
+	bi.biHeight = -height;
+	bi.biPlanes = 1;
+	bi.biBitCount = 32;
+	bi.biCompression = BI_RGB;
+	bi.biSizeImage = 0; // No compression
+	bi.biXPelsPerMeter = 1;
+	bi.biYPelsPerMeter = 2;
+	bi.biClrUsed = 3;
+	bi.biClrImportant = 4;
+
+	Mat mat = Mat(height, width, CV_8UC4); // 8 bit unsigned ints 4 channels -> RGBA
+
+	// Transform data and store into mat.data
+	GetDIBits(memoryDeviceContext, bitmap, 0, height, mat.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);
+
+	// Clean up
+	DeleteObject(bitmap);
+	DeleteDC(memoryDeviceContext);
+	ReleaseDC(hwnd, deviceContext);
+
+	return mat;
+}
+
+
+void main() {
+
+	LPCWSTR windowTitle = L"Spotify Free";
+
+	HWND hwnd = FindWindow(NULL, windowTitle);
+	while (!hwnd) {
+		system("cls");
+		cout << "Start" << endl;
+		Sleep(100);
+	}
+
+	// Faster to create window
+	namedWindow("output", WINDOW_NORMAL);
+
+	while (true) {
+		Mat img = getMat(hwnd);
+
+		imshow("Output", img);
+		waitKey(30);
+	}
+}
